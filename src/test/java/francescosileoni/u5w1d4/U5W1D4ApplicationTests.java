8adda660d5d2a291e0fa5b8a340efcd7b2637eea
Francesco Sileoni <francesco.sile@gmail.com>
2024-04-05 16:47:57 +0200

    .

---
+++ src/test/java/francescosileoni/u5w1d4/U5W1D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package francescosileoni.u5w1d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
